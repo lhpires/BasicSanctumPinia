fc28ebb302c4513a385c97a1eb323ca54df5973f
lhpires <lucas@pires.dev.br>
2024-01-26 15:51:23 -0300

    remove console log

---
+++ backend/routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\UserAuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/login', [UserAuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::post('/logout', [UserAuthController::class, 'logout']);
+
+    Route::get('/validateAuth', [UserAuthController::class, 'validateToken']);
+});
